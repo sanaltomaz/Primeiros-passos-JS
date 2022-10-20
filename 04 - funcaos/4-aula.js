c63c885a11fbcd3ac7ae22395c350306b2607d31
sanaltomaz <tomazsanal@gmail.com>
2022-10-20 18:36:28 -0300

    Conhecendo funções, aula-1

---
+++ 04 - funcaos/4-aula.js
@@ -0,0 +1,24 @@
+/*
+function sayMyName(name,){
+    console.log('Your name is ' + name);
+}
+
+sayMyName('Sanal');
+*/
+
+/*
+function incrementarJuros(valor, percentualJuros){
+    const valorDeAcrecimo = (percentualJuros / 100) *valor;
+    return valor + valorDeAcrecimo;
+}
+console.log(incrementarJuros(100, 10));
+*/
+
+function calcularJuros(){
+
+}
+
+function main(){
+    console.log('Programa principal');
+    calcularJuros();
+}
